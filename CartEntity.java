80b129264e6c5f0deedc3166b9b073ea43b8e1b9
Manikanta <105136680+Manidgyf@users.noreply.github.com>
2024-09-11 15:31:07 +0530

    Update CartEntity.java

--- CartEntity.java
+++ CartEntity.java
@@ -2,6 +2,6 @@ public class CartEntity
 {
 	public static void main(String args[])
 	{
-		System.out.println("welcome to jenkins ra howlee "+args[0]);
+		System.out.println("welcome to "+args[0]);
 	}
 }
